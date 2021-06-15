ad2853dbb365ec683355f2a4f6b2e414b2b65301
Amresh Tripathy <amreshtripathy184@gmail.com>
2021-06-15 21:30:30 +0530

    Add files via upload

---
+++ 05_concatenation.dart
@@ -0,0 +1,8 @@
+void main() {
+  var name = 'Amresh';
+  var lastName = 'Tripathy';
+  int age = 18;
+
+  print("$name $lastName's age is $age");
+  print('Hello ${name.toUpperCase()} ${lastName.toLowerCase()}!');
+}
