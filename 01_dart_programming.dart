7679361ea20eefd367249a9995ccc95e64a58c3f
Amresh Tripathy <amreshtripathy184@gmail.com>
2021-06-15 21:29:53 +0530

    Add files via upload

---
+++ 01_dart_programming.dart
@@ -0,0 +1,13 @@
+void main(List<String> arguments) {
+  var a;
+  a = 'amresh';
+  a = 123;
+  String b;
+  b = 'tripathy';
+
+  print(a);
+  print(b);
+  for (var i = 0; i < 5; i++) {
+    print('Hello ${i + 1}');
+  }
+}
