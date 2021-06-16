c7271353e901a03eebb934205f75ee3589c97818
Amresh Tripathy <amreshtripathy184@gmail.com>
2021-06-16 20:46:30 +0530

    Add files via upload

---
+++ 09_if_statement.dart
@@ -0,0 +1,13 @@
+void main() {
+  var num1, num2;
+  num1 = 543;
+  num2 = 432;
+
+  // if (num1 > num2) {
+  //   print('$num1 is greater than $num2');
+  // } else {
+  //   print('$num2 is grater than $num1');
+  // }
+  if (num1 > num2) print('$num1 is greater than $num2');
+  else print('$num2 is grater than $num1');
+}
