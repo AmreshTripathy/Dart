6c1d7972a8cbfdc56201a7f409430cbd5ce6bcfe
Amresh Tripathy <amreshtripathy184@gmail.com>
2021-06-16 20:46:07 +0530

    Add files via upload

---
+++ 06_arithematic.dart
@@ -0,0 +1,13 @@
+void main() {
+  int number;
+  number = 10;
+  int numberTwo;
+  numberTwo = 2;
+  var result = number + numberTwo;
+  // print(number + numberTwo);
+  print(result);
+  double pi;
+  pi = 3.14;
+  var result2 = pi * number;
+  print(result2);
+}
