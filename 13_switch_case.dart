197456b79954b341a85e5608a0f1ddc846f648eb
Amresh Tripathy <amreshtripathy184@gmail.com>
2021-06-17 23:07:06 +0530

    Add files via upload

---
+++ 13_switch_case.dart
@@ -0,0 +1,14 @@
+void main() {
+  var age;
+  age = 18;
+  switch (age) {
+    case 50:
+      print('YOu are now adult');
+      break;
+    case 20:
+      print('You are elder');
+      break;
+    default:
+      print('Thank you for your response');
+  }
+}
